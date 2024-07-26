a8a2c6454f1640ac4f89b784ea113fe246fed809
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-07-26 19:16:16 +1200

    ui updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,44 @@
 import MyCard from "@/components/Card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { projects } from "./content";
+import { projects, skills } from "./content";
+import Pill from "@/components/Pill";
 
 export default function Home() {
   return (
     <>
       <main className="flex h-full flex-col items-center justify-between">
         <section className="flex flex-col gap-5 items-center">
-          <div className="w-full mt-20 flex items-center">
-            <Avatar className="h-[100px] w-[100px] md:h-[200px] md:w-[200px]">
-              <AvatarImage src="https://github.com/MVAodhan.png" />
+          <div className="w-full mt-20 flex items-center flex-row-reverse">
+            <Avatar className="h-[50px] w-[50px] sm:h-[150px] sm:w-[150px]">
+              <AvatarImage src="/avatar.png" />
               <AvatarFallback>AH</AvatarFallback>
             </Avatar>
-            <p className="ml-5 text-sm sm:text-base md:text-lg md:m-0 md:px-20">
+            <p className="text-base sm:text-lg md:text-2xl md:m-0 px-5 md:px-20">
               {" "}
-              Kia Ora, I&apos;m Aodhan Hamilton, a Software Developer and a
-              part-time Digital Assistant.{" "}
+              Kia Ora, I&apos;m Aodhan Hamilton, a Sull Stack Software Developer
+              and a part-time Digital Assistant.{" "}
             </p>
           </div>
-          <h2 className="text-3xl">Work and Projects</h2>
-          <div className="w-full rounded flex flex-col gap-5">
-            {projects.map((project, i) => (
-              <MyCard
-                key={i}
-                title={project.title}
-                description={project.description}
-                url={project.url}
-              />
-            ))}
+          <div className="w-full flex flex-col items-center gap-2 m-5">
+            <h2 className="text-3xl font-bold">Skills</h2>
+            <div className="w-full flex flex-wrap gap-2 m-5  justify-center">
+              {skills.map((skill, i) => (
+                <Pill key={i} name={skill.name} />
+              ))}
+            </div>
           </div>
         </section>
+        <h2 className="text-3xl font-bold">Work and Projects</h2>
+        <div className="w-full rounded flex flex-col gap-5">
+          {projects.map((project, i) => (
+            <MyCard
+              key={i}
+              title={project.title}
+              description={project.description}
+              url={project.url}
+            />
+          ))}
+        </div>
       </main>
     </>
   );
