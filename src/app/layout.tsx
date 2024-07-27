32cfd1a242766a0bbf81cabdb5022b4aae3068ac
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-07-27 20:30:58 +1200

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/theme-toggle";
 import Nav from "@/components/Nav";
 const inter = Inter({ subsets: ["latin"] });
 
