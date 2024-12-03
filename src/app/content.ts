0955f5a45d171bc3eaceafe6644e05f0ac20682d
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-12-03 15:47:20 +1300

    add project

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,60 +1,65 @@
 export const projects = [
   {
-    title: "Plus 91 Cafe",
-    description: "A client website for a local cafe",
-    url: "https://www.plus91cafe.co.nz",
+    title: 'SnippetShare',
+    description: 'A work in Progress. A development platform for developers to share code snippets',
+    url: 'https://dulcet-biscotti-935299.netlify.app/',
   },
   {
-    title: "Schedule with Jason",
-    description:
-      "An internal tool and hub to schedule Learn with Jason episode, aggregate data and generate content ",
-    url: "https://www.schedulewithjason.page/",
+    title: 'Plus 91 Cafe',
+    description: 'A client website for a local cafe',
+    url: 'https://www.plus91cafe.co.nz',
   },
-];
+  {
+    title: 'Schedule with Jason',
+    description:
+      'An internal tool and hub to schedule Learn with Jason episode, aggregate data and generate content ',
+    url: 'https://www.schedulewithjason.page/',
+  },
+]
 
 export const skills = [
   {
-    name: "HTML",
+    name: 'HTML',
   },
   {
-    name: "CSS",
+    name: 'CSS',
   },
   {
-    name: "Tailwind CSS",
+    name: 'Tailwind CSS',
   },
   {
-    name: "JavaScript",
+    name: 'JavaScript',
   },
 
   {
-    name: "TypeScript",
+    name: 'TypeScript',
   },
   {
-    name: "React",
+    name: 'React',
   },
   {
-    name: "Next.js",
+    name: 'Next.js',
   },
   {
-    name: "Node.js",
+    name: 'Node.js',
   },
   {
-    name: "Postgresql",
+    name: 'Postgresql',
   },
   {
-    name: "Serverless",
+    name: 'Serverless',
   },
-];
+]
 
-export const devToUsername: string = "mvaodhan";
+export const devToUsername: string = 'mvaodhan'
 
 export const navBarLinks = [
   {
-    title: "Home",
-    url: "/",
+    title: 'Home',
+    url: '/',
   },
   {
-    title: "Blog",
-    url: "/blog",
+    title: 'Blog',
+    url: '/blog',
   },
-];
+]
